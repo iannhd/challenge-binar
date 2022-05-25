8f955d9543fb1b35470fa2683ea82f3c12981376
“Hafizh24” <hafizhcbs@gmail.com>
2022-05-25 13:33:31 +0700

    add features register & login

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Header(props) {
+    return(
+        <nav class="navbar navbar-dark bg-primary">
+            <div className="row col-12 d-flex justify-content-center text-white">
+            <span className="h3">{props.title}</span>
+            </div>
+        </nav>
+    )
+}
+export default Header;
---
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Header from '../../Component/Header/Header';
+export default function RegisterPage() {
+  return(
+        <>
+        <Header title="Register"/>
+        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+            <form>
+                <div className="form-group text-left">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" 
+                       className="form-control" 
+                       id="email" 
+                       aria-describedby="emailHelp" 
+                       placeholder="Enter email"
+                />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group text-left">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" 
+                        className="form-control" 
+                        id="password" 
+                        placeholder="Password"
+                    />
+                </div>
+                <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                >
+                    Register
+                </button>
+            </form>
+        </div>
+        </>
+    )
+}
